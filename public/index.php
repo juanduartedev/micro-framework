87915a9ad0e8420fbaa633cb0539602dd7d8ad04
juanduartedev <juanduartedev@gmail.com>
2025-11-26 07:46:53 -0300

    Proyecto reemplazado completamente

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Cargar las clases en todos los documentos
-require_once '/../autoload.php';
-
-
-//Cargar el documento web.php para poder cargar y redirigir las vistas
-require_once '/../routes/web.php';
